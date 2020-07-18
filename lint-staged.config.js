879ee287e260d43282f5a25e106abe01cc9e789c
Harry Tran <nhattq.coding@gmail.com>
2020-07-19 02:08:24 +0700

    chore: update git hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,6 @@ module.exports = {
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['yarn lint:prettier --parser json'],
   'package.json': ['yarn lint:prettier'],
   '*.vue': ['yarn lint:files', 'yarn lint:eslint', 'yarn lint:stylelint', 'yarn lint:prettier'],
-  '*.{css,scss}': ['yarn lint:css', 'yarn lint:prettier'],
+  '*.{css,scss}': ['yarn lint:prettier'],
   '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged'],
 };
