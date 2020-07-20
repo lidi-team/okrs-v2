1694d403cd90def82f9805e6cede2f394db5a50b
Harry Tran <nhattq.coding@gmail.com>
2020-07-20 18:17:05 +0700

    feat: complete reset password with token screen

---
+++ components/account/ResetPassword.vue
@@ -0,0 +1,155 @@
+<template>
+  <el-row class="reset-password-form" type="flex" justify="space-between">
+    <div class="reset-password-form__title">
+      <span>Đặt lại mật khẩu</span>
+    </div>
+    <el-form
+      ref="resetPasswordForm"
+      :model="resetPasswordForm"
+      :rules="rules"
+      :status-icon="true"
+      :hide-required-asterisk="false"
+      label-width="150px"
+      label-position="top"
+      @submit.native.prevent="handleSubmit"
+    >
+      <div class="reset-password-form__input">
+        <el-form-item prop="newPassword" label="Mật khẩu mới">
+          <el-input
+            v-model="resetPasswordForm.newPassword"
+            type="password"
+            class="reset-password-form__input__new-password"
+            placeholder="Nhập mật khẩu"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="matchPassword" label="Nhập lại mật khẩu mới">
+          <el-input
+            v-model="resetPasswordForm.matchPassword"
+            type="password"
+            class="reset-password-form__input__match-password"
+            placeholder="Nhập mật khẩu"
+          ></el-input>
+        </el-form-item>
+      </div>
+      <el-row type="flex" justify="space-between" class="reset-password-form__button">
+        <el-col :span="12">
+          <el-button class="el-button el-button--white el-button--medium" @click="returnLoginPage">Quay lại trang chủ</el-button>
+        </el-col>
+        <el-col :span="12">
+          <el-button :loading="loading" class="el-button el-button--purple el-button--medium" @click="handleSubmit">Đổi mật khẩu</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+  </el-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from 'nuxt-property-decorator';
+import { Form as resetPasswordForm } from 'element-ui';
+import { ResetPasswordDTO } from '@/constants/app.interface';
+import { Maps, Rule } from '@/constants/app.type';
+@Component<ResetPassword>({
+  name: 'ResetPassword',
+})
+export default class ResetPassword extends Vue {
+  @Prop({ default: false }) public loading!: boolean;
+  watchQuery: string[] = ['token'];
+
+  public resetPasswordForm: ResetPasswordDTO = {
+    newPassword: '',
+    matchPassword: '',
+    token: '',
+  };
+
+  private rules: Maps<Rule[]> = {
+    newPassword: [
+      { required: true, message: 'Vui lòng nhập mật khẩu mới', trigger: 'blur' },
+      { validator: this.validatePassword, trigger: ['blur', 'change'] },
+    ],
+    matchPassword: [
+      { required: true, message: 'Vui lòng nhập lại mật khẩu' },
+      { validator: this.validateMatchPassword, trigger: ['blur', 'change'] },
+    ],
+  };
+
+  private validatePassword(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
+    const valid: boolean = /^(?=.*\d)[0-9a-zA-Z]{8,}$/.test(value);
+    if (!valid) {
+      return callback('Mật khẩu chứ ít nhất 8 ký tự và 1 chữ số');
+    }
+    return callback();
+  }
+
+  private validateMatchPassword(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
+    if (value !== this.resetPasswordForm.newPassword) {
+      return callback('Không trùng với mật khẩu mới');
+    }
+    return callback();
+  }
+
+  private returnLoginPage(): void {
+    this.$router.push('/dang-nhap');
+  }
+
+  @Emit('submit')
+  private handleSubmit(): ResetPasswordDTO {
+    const theForm = this.$refs.resetPasswordForm as resetPasswordForm;
+    let result: any;
+    theForm.validate((valid) => {
+      if (valid) {
+        this.resetPasswordForm.token = this.$route.query.token as string;
+        result = this.resetPasswordForm;
+      }
+    });
+    return result;
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/main.scss';
+.reset-password-form {
+  flex-direction: column;
+  padding: $unit-12;
+  box-shadow: 0px 1px 3px rgba(63, 63, 68, 0.15), 0px 0px 0px rgba(63, 63, 68, 0.05);
+  @include breakpoint-down(phone) {
+  }
+  &__title {
+    text-align: center;
+    border: 10px;
+    font-size: 1.75rem;
+    color: $neutral-primary-4;
+  }
+  &__input {
+    &__new-password {
+    }
+    &__match-password {
+    }
+  }
+  &__button {
+    margin-top: $unit-10;
+  }
+  .el-form-item {
+    &:first-child {
+      margin-top: $unit-4;
+    }
+    &__label {
+      color: $neutral-primary-4;
+      padding-bottom: $unit-1;
+    }
+    .el-input__inner {
+      &::placeholder {
+        color: $neutral-primary-1;
+      }
+    }
+  }
+  .el-row {
+    .el-col:nth-child(2) {
+      text-align: right;
+    }
+  }
+}
+.el-button {
+  font-size: $unit-5;
+}
+</style>
