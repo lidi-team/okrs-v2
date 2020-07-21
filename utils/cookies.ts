0e16ac050f9733f1c210853ae61b963dd150e050
Harry Tran <nhattq.coding@gmail.com>
2020-07-21 15:25:24 +0700

    feat: add js cookies

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -2,6 +2,6 @@ import Cookies from 'js-cookie';
 
 // User token default
 const tokenKey = 'vue_typescript_admin_access_token';
-export const getToken = () => Cookies.get(tokenKey);
-export const setToken = (token: string) => Cookies.set(tokenKey, token);
-export const removeToken = () => Cookies.remove(tokenKey);
+export const getTokenCookies = (): string | undefined => Cookies.get(tokenKey);
+export const setTokenCookies = (token: string): string | undefined => Cookies.set(tokenKey, token);
+export const removeTokenCookies = (): void => Cookies.remove(tokenKey);
