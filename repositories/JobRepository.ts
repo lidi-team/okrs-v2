fb88f695afb2bae72ba29c0da48d4e7414c919a4
Harry Tran <nhattq.coding@gmail.com>
2020-07-28 17:40:16 +0700

    chore: add team & job repository

--- repositories/JobRepository.ts
+++ repositories/JobRepository.ts
@@ -1,8 +1,22 @@
-import { unAuthenticatedservice } from './BaseRepository';
+import { AxiosResponse } from 'axios';
+import { unAuthenticatedservice, authenticatedService } from './BaseRepository';
 import { ResourcesEnpoint } from '@/constants/app.enum';
+import { JobPositionDTO } from '@/constants/app.interface';
 
 export default class JobRepository {
   public static get() {
     return unAuthenticatedservice.get(`${ResourcesEnpoint.Job}`);
+  }
+
+  public static update(payload: JobPositionDTO): Promise<AxiosResponse<any>> {
+    return authenticatedService.put(`${ResourcesEnpoint.Job}/${payload.id}`, payload);
+  }
+
+  public static post(payload: JobPositionDTO): Promise<AxiosResponse<any>> {
+    return authenticatedService.post(`${ResourcesEnpoint.Job}`, payload);
+  }
+
+  public static delete(id: number): Promise<AxiosResponse<any>> {
+    return authenticatedService.delete(`${ResourcesEnpoint.Job}/${id}`);
   }
 }
--- repositories/TeamRepository.ts
+++ repositories/TeamRepository.ts
@@ -1,8 +1,22 @@
-import { unAuthenticatedservice } from './BaseRepository';
+import { AxiosResponse } from 'axios';
+import { unAuthenticatedservice, authenticatedService } from './BaseRepository';
 import { ResourcesEnpoint } from '@/constants/app.enum';
+import { TeamDTO } from '@/constants/app.interface';
 
 export default class TeamRepository {
   public static get() {
     return unAuthenticatedservice.get(`${ResourcesEnpoint.Team}`);
+  }
+
+  public static update(payload: TeamDTO): Promise<AxiosResponse<any>> {
+    return authenticatedService.put(`${ResourcesEnpoint.Team}/${payload.id}`, payload);
+  }
+
+  public static post(payload: TeamDTO): Promise<AxiosResponse<any>> {
+    return authenticatedService.post(`${ResourcesEnpoint.Team}`, payload);
+  }
+
+  public static delete(id: number): Promise<AxiosResponse<any>> {
+    return authenticatedService.delete(`${ResourcesEnpoint.Team}/${id}`);
   }
 }
