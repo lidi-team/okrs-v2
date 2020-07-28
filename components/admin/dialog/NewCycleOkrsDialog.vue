5455a886bed27adec2912e0bd00f8a63fadda5c2
Harry Tran <nhattq.coding@gmail.com>
2020-07-28 17:42:02 +0700

    feat: add add new cycle OKRs dialog

---
+++ components/admin/dialog/NewCycleOkrsDialog.vue
@@ -0,0 +1,126 @@
+<template>
+  <el-dialog
+    title="Thêm mới chu kỳ"
+    :visible.sync="syncCycleDialog"
+    width="30%"
+    placement="center"
+    :before-close="handleCloseDialog"
+    class="dialog-cycle-okrs"
+  >
+    <el-row>
+      <el-col :span="24">
+        <el-form ref="temCreateCycle" :model="temCreateCycle" :rules="rules">
+          <el-form-item label="Tên chu kỳ" prop="name" class="custom-label" label-width="120px">
+            <el-input v-model="temCreateCycle.name" placeholder="Nhập tên chu kỳ" />
+          </el-form-item>
+          <el-form-item label="Ngày bắt đầu" prop="startDate" class="custom-label" label-width="120px">
+            <el-date-picker
+              v-model="temCreateCycle.startDate"
+              type="date"
+              placeholder="Chọn ngày bắt đầu"
+              :format="dateFormat"
+              :value-format="dateFormat"
+            ></el-date-picker>
+          </el-form-item>
+          <el-form-item label="Ngày kết thúc" prop="endDate" class="custom-label" label-width="120px">
+            <el-date-picker
+              v-model="temCreateCycle.endDate"
+              type="date"
+              placeholder="Chọn ngày kết thúc"
+              :format="dateFormat"
+              :value-format="dateFormat"
+            ></el-date-picker>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <span slot="footer">
+      <el-button class="el-button--white el-button--modal" @click="handleCloseDialog">Hủy</el-button>
+      <el-button :loading="loading" class="el-button--purple el-button--modal" @click="createCycleOkrs">Thêm mới</el-button>
+    </span>
+  </el-dialog>
+</template>
+<script lang="ts">
+import { Component, Vue, PropSync } from 'vue-property-decorator';
+import { Form } from 'element-ui';
+import { CycleDTO } from '@/constants/app.interface';
+import { Maps, Rule } from '@/constants/app.type';
+import { compareTwoDate, formatDateToYYYY } from '@/utils/dateParser';
+import CycleRepository from '@/repositories/CycleRepository';
+@Component<CycleOkrsDialog>({
+  name: 'CycleOkrsDialog',
+})
+export default class CycleOkrsDialog extends Vue {
+  @PropSync('cycleVisibleDialog', { type: Boolean, required: true }) public syncCycleDialog!: boolean;
+
+  private loading: boolean = false;
+  private dateFormat: string = 'dd/MM/yyyy';
+  private temCreateCycle: CycleDTO = {
+    name: '',
+    startDate: null,
+    endDate: null,
+  };
+
+  private rules: Maps<Rule[]> = {
+    name: [
+      { type: 'string', required: true, message: 'Vui lòng nhập tên chu kỳ', trigger: 'blur' },
+      { min: 3, message: 'Tên chu kỳ chứa ít nhất 3 ký tự' },
+    ],
+    startDate: [{ required: true, message: 'Vui lòng chọn ngày bắt đầu', trigger: 'blur' }],
+    endDate: [
+      { required: true, message: 'Vui lòng chọn ngày kết thúc', trigger: 'blur' },
+      { validator: this.validateEndDate, trigger: ['blur', 'change'] },
+    ],
+  };
+
+  private validateEndDate(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
+    if (compareTwoDate(value, this.temCreateCycle.startDate) === 1) {
+      return callback('Ngày kết thúc phải lớn hơn ngày bắt đầu');
+    }
+    return callback();
+  }
+
+  private createCycleOkrs() {
+    this.loading = true;
+    (this.$refs.temCreateCycle as Form).validate(async (isValid) => {
+      try {
+        const tempCycle: CycleDTO = {
+          name: this.temCreateCycle.name,
+          startDate: formatDateToYYYY(this.temCreateCycle.startDate),
+          endDate: formatDateToYYYY(this.temCreateCycle.endDate),
+        };
+        await CycleRepository.post(tempCycle).then((res) => {
+          this.$notify({
+            title: 'Status',
+            message: `Tạo mới thành công chu kỳ ${res.data.data.name}`,
+            type: 'success',
+            duration: 2000,
+          });
+        });
+        this.loading = false;
+        this.syncCycleDialog = false;
+      } catch (error) {
+        this.$notify({
+          title: 'Lỗi',
+          message: `Lỗi ${error.message}`,
+          type: 'error',
+          duration: 2000,
+        });
+        this.loading = false;
+      }
+    });
+  }
+
+  private handleCloseDialog() {
+    (this.$refs.temCreateCycle as Form).clearValidate();
+    this.syncCycleDialog = false;
+  }
+}
+</script>
+<style lang="scss" scoped>
+.dialog-cycle-okrs {
+  .el-date-editor.el-input {
+    width: 100%;
+  }
+}
+</style>
