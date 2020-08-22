fc2da7c2f1b860ffd6dd215752bf2e0f33894bdf
bacodekiller <bacodekiller@gmail.com>
2020-08-23 00:04:49 +0700

    feat: create upload avatar language vi

---
+++ utils/language.js
@@ -0,0 +1,40 @@
+export default {
+  en: {
+    hint: 'Click or drag the file here to upload',
+    loading: 'Uploading…',
+    noSupported: 'Browser is not supported, please use IE10+ or other browsers',
+    success: 'Upload success',
+    fail: 'Upload failed',
+    preview: 'Preview',
+    btn: {
+      off: 'Cancel',
+      close: 'Close',
+      back: 'Back',
+      save: 'Save',
+    },
+    error: {
+      onlyImg: 'Image only',
+      outOfSize: 'Image exceeds size limit: ',
+      lowestPx: "Image's size is too low. Expected at least: ",
+    },
+  },
+  vi: {
+    hint: 'Nhấp hoặc kéo tệp vào đây để tải lên',
+    loading: 'Đang xử lý…',
+    noSupported: 'Trình duyệt không được hỗ trợ, vui lòng sử dụng IE10 + hoặc các trình duyệt khác',
+    success: 'Tải lên thành công',
+    fail: 'Tải lên thất bại',
+    preview: 'Xem trước',
+    btn: {
+      off: 'Hủy',
+      close: 'Đóng',
+      back: 'Quay lại',
+      save: 'Lưu',
+    },
+    error: {
+      onlyImg: 'Chỉ cho phép tập tin hình ảnh',
+      outOfSize: 'Hình ảnh vượt quá giới hạn kích thước: ',
+      lowestPx: ' Kích thước của hình ảnh quá thấp. Kích thước thấp nhất: ',
+    },
+  },
+};
