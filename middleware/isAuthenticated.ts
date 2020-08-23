3d6d02d6cb60bffbb291befcfe5f93574968f132
Harry Tran <nhattq.coding@gmail.com>
2020-08-23 13:30:19 +0700

    feat: update middleware

--- middleware/isAuthenticated.ts
+++ middleware/isAuthenticated.ts
@@ -4,21 +4,21 @@ import CycleRepository from '@/repositories/CycleRepository';
 import { DispatchAction, MutationState } from '@/constants/app.enum';
 
 export default async function ({ redirect, store }) {
-  const token = getTokenCookie();
-  if (!token) {
-    store.dispatch(DispatchAction.CLEAR);
-    return redirect('/dang-nhap');
-  } else {
-    store.commit(MutationState.SET_TOKEN, token);
-    if (store.state.auth.user === null) {
-      try {
+  try {
+    const token = getTokenCookie();
+    if (!token) {
+      store.dispatch(DispatchAction.CLEAR);
+      return redirect('/dang-nhap');
+    } else {
+      store.commit(MutationState.SET_TOKEN, token);
+      if (store.state.auth.user === null) {
         const [user, cycle] = await Promise.all([UserRepository.me(), CycleRepository.getCurrentCycle()]);
         store.commit(MutationState.SET_USER, user.data.data);
         store.commit(MutationState.SET_CURRENT_CYCLE, cycle.data.data);
-      } catch (error) {
-        store.dispatch(DispatchAction.CLEAR);
-        return redirect('/dang-nhap');
       }
     }
+  } catch (error) {
+    store.dispatch(DispatchAction.CLEAR);
+    return redirect('/dang-nhap');
   }
 }
