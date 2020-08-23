204d7d2f03d5d4b37f4a2c42af764242130f6254
Harry Tran <nhattq.coding@gmail.com>
2020-08-23 16:43:30 +0700

    237 fix: remove amchart logo

--- components/checkin/ChartOkrs.vue
+++ components/checkin/ChartOkrs.vue
@@ -106,6 +106,7 @@ export default class ChartOkrs extends Vue {
 
     chart.cursor = new am4charts.XYCursor();
     chart.cursor.xAxis = dateAxis;
+    chart.logo.disabled = true;
   }
 }
 </script>
