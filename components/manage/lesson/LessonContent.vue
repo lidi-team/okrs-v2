19289290e9c4323b9d8855fe34c94ab5697776f0
bacodekiller <bacodekiller@gmail.com>
2020-08-25 22:45:33 +0700

    chore: set logo capi

--- components/manage/lesson/LessonContent.vue
+++ components/manage/lesson/LessonContent.vue
@@ -4,11 +4,7 @@
     <h1 class="lesson-content__title">{{ post.title }}</h1>
     <div class="lesson-content__des">
       <div>
-        <img
-          alt="avatar-logo"
-          class="lesson-content__avatar"
-          src="https://avatars3.githubusercontent.com/u/23659823?s=460&u=cbb3ca39bc2e9969614339f47ff3743f688f1a11&v=4"
-        />
+        <img alt="avatar-logo" class="lesson-content__avatar" src="@/static/icon.png" />
       </div>
       <div class="lesson-content__author">
         <span class="lesson-content__name">Capi Creative</span>
@@ -72,9 +68,10 @@ export default class LessonContent extends Vue {
     display: flex;
   }
   &__avatar {
+    background-color: #e2e8f0;
     display: block;
-    width: 3rem;
-    height: 3rem;
+    width: 2.5rem;
+    height: 2.5rem;
     border-radius: 100%;
   }
   &__author {
