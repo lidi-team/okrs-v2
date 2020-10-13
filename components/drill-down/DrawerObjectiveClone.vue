7f0e331876d0c7aa7fefdbd3f3a30ec23cba8a5b
thaycacac <thaycacac@gmail.com>
2020-10-13 23:44:47 +0700

    update: drilldown

--- components/drill-down/DrawerObjectiveClone.vue
+++ components/drill-down/DrawerObjectiveClone.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-table :data="tableData" style="width: 100%;">
+    <el-table :data="dataChildrenUpdated" style="width: 100%;">
       <el-table-column prop="id" label="ID" width="100" align="center"> </el-table-column>
       <el-table-column prop="name" label="Name"> </el-table-column>
       <el-table-column prop="author" label="Author" width="250"></el-table-column>
@@ -20,18 +20,18 @@
         </template>
       </el-table-column>
     </el-table>
-    <el-drawer :visible.sync="selected" size="80%" :append-to-body="true">
-      <DrawerObjective :id-selected="idSelected" :data-children="dataChildren" :width="80" />
+    <el-drawer :visible.sync="selected" :append-to-body="true" :size="`${width - 10}%`">
+      <DrawerObjective :id-selected="idSelectedChild" :data-children="dataGrandChildren" :width="width - 10" />
     </el-drawer>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator';
-import DrawerObjective from '@/components/drill-down/DrawerObjective.vue';
+import { Component, Vue, Watch, Prop } from 'vue-property-decorator';
 import DrillDownRepository from '@/repositories/DrillDownRepository';
+import DrawerObjective from '@/components/drill-down/DrawerObjective.vue';
 
-@Component<DrillDownPage>({
+@Component<DrawerObjectiveClone>({
   name: 'DrillDownPage',
   filters: {
     filterType(value: Number) {
@@ -48,21 +48,31 @@ import DrillDownRepository from '@/repositories/DrillDownRepository';
     DrawerObjective,
   },
   async mounted() {
-    const { data } = await DrillDownRepository.get(0);
-    this.tableData = data;
-    console.log(data);
+    const { data } = await DrillDownRepository.get(this.idSelected);
+    this.dataChildrenUpdated = data;
   },
 })
-export default class DrillDownPage extends Vue {
-  private selected: boolean = false;
-  private idSelected: Number = 0;
-  private dataChildren: any = {};
-  private tableData: any = [];
+export default class DrawerObjectiveClone extends Vue {
+  @Prop(Array) public dataChildren!: [];
+  @Prop(Number) public idSelected!: 0;
+  @Prop(Number) public width!: 0;
+  private selected: Boolean = false;
+  private idSelectedChild: Number = 0;
+  private dataGrandChildren: any = {};
+
+  private dataChildrenUpdated: any = this.dataChildren;
+
+  @Watch('idSelected')
+  private async getData(id: Number) {
+    const { data } = await DrillDownRepository.get(id);
+    this.dataChildrenUpdated = data;
+  }
 
   private drillDown(data) {
+    console.log(data);
     this.selected = true;
-    this.idSelected = data.id;
-    this.dataChildren = data.grandChildObjectives;
+    this.idSelectedChild = data.id;
+    this.dataGrandChildren = data.grandChildObjectives;
   }
 }
 </script>
