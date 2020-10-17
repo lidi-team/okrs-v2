e96e401f004290291e3ae1007bb11aea763765f9
thaycacac <thaycacac@gmail.com>
2020-10-17 16:41:41 +0700

    feat: show krs

---
+++ components/okrs/items/kr.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-table :data="keyResults" header-row-class-name="krs-detail__table-header">
+    <el-table-column label="KRs" min-width="300">
+      <template v-slot="{ row }">
+        <span class="krs-detail__content">{{ row.content }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Tiến độ" width="150">
+      <template v-slot="{ row }">
+        <div class="krs-detail__progress">
+          <el-progress style="width: 80%;" :percentage="getProgressKrs(row)" :color="customColors" :text-inside="true" :stroke-width="20" />
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column label="Link kế hoạch" width="150">
+      <template v-slot="{ row }">
+        <a class="krs-detail__link" :href="`${row.linkPlans}`" target="_blank">
+          {{ row.linkPlans }}
+        </a>
+      </template>
+    </el-table-column>
+    <el-table-column label="Link kết quả" width="150">
+      <template v-slot="{ row }">
+        <a class="krs-detail__link" :href="`${row.linkPlans}`" target="_blank">
+          {{ row.linkPlans }}
+        </a>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+<script lang="ts">
+import { Component, Vue, PropSync, Prop } from 'vue-property-decorator';
+import { customColors } from '../okrs.constant';
+@Component<DetailKrsDialog>({ name: 'DetailKrsDialog' })
+export default class DetailKrsDialog extends Vue {
+  @Prop({ type: Array, required: true }) public keyResults!: any[];
+  private customColors = customColors;
+  private getProgressKrs(krs: any) {
+    return Math.floor(krs.valueObtained / krs.targetValue) * 100;
+  }
+}
+</script>
+<style lang="scss">
+@import '@/assets/scss/main.scss';
+.krs-detail {
+  &__header {
+    color: $neutral-primary-4;
+  }
+  &__table-header {
+    color: $neutral-primary-4;
+    font-size: $unit-4;
+  }
+  &__content {
+    word-break: break-word;
+  }
+  &__progress {
+    width: 150px;
+  }
+  &__link {
+    color: $blue-primary-2;
+    @include text-ellipsis(1);
+  }
+  .el-progress {
+    width: 80%;
+    .el-progress-bar {
+      &__outer {
+        background-color: $purple-primary-2;
+        border-radius: $border-radius-medium;
+        .el-progress-bar__inner {
+          border-radius: $border-radius-medium;
+        }
+      }
+    }
+  }
+  .el-dialog {
+    &__header {
+      padding: $unit-4 $unit-7;
+    }
+    &__body {
+      padding: 0 $unit-5 $unit-5;
+    }
+  }
+}
+</style>
--- pages/demo.vue
+++ pages/demo.vue
@@ -1,99 +1,21 @@
 <template>
   <div>
-    <el-table :data="dataObjectives" style="width: 100%;">
-      <el-table-column label="Mục tiêu">
-        <template v-slot="{ row }">
-          <span>{{ row.title }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="author" label="Kết quả then chốt" width="250" align="center">
-        <template v-slot="{ row }">
-          <span>{{ row.keyResults | filterKeyresults }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="author" label="Tiến độ" width="250" align="center">
-        <template v-slot="{ row }">
-          <div class="item__progress">
-            <el-progress :percentage="+row.progress" :color="customColors" :text-inside="true" :stroke-width="26" />
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column label="Thay đổi" width="250" align="center">
-        <template v-slot="{ row }">
-          <div class="item__action">
-            <p :class="row.changing | getStatusOfProgress">{{ row.changing }}%</p>
-            <!-- <okrs-action-tooltip
-              :reload-data="reloadData"
-              :editable="editableOkrs(row.user.id)"
-              :okrs-id.sync="row.id"
-              :temp-okrs="row"
-              :is-root-okrs="row.isRootObjective"
-              @updateTempOkrs="updateTempOkrs($event)"
-            /> -->
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column label="Loại" width="200" prop="type" align="center"> </el-table-column>
-      <el-table-column label="Hành động" align="right" width="80">
-        <template v-slot="{ row }">
-          <el-button
-            type="primary"
-            icon="el-icon-arrow-right"
-            class="el-button el-button--purple el-button--small"
-            @click="drillDown(row)"
-          ></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-drawer :visible.sync="selected" size="80%" :append-to-body="true">
-      <DrawerObjective :id-selected="idSelected" width="80" />
-    </el-drawer>
+    <p>hello</p>
+    <ItemOkrs />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
-import DrawerObjective from '@/components/drill-down/DrawerObjective.vue';
-import DrillDownRepository from '@/repositories/DrillDownRepository';
-import { filterKeyresults } from '@/utils/filters';
-import { customColors, getStatusOfProgress } from '@/utils/common';
+import ItemOkrs from '@/components/okrs/items/index.vue';
 
 @Component<DrillDownPage>({
   name: 'DrillDownPage',
   components: {
-    DrawerObjective,
-  },
-  filters: {
-    filterKeyresults,
-    getStatusOfProgress,
-  },
-  async mounted() {
-    const { data } = await DrillDownRepository.get(3, 0);
-    this.dataObjectives = data;
+    ItemOkrs,
   },
 })
-export default class DrillDownPage extends Vue {
-  private selected: boolean = false;
-  private idSelected: Number = 0;
-  private dataObjectives: any = [];
-  private customColors = customColors;
-
-  private drillDown(data) {
-    this.selected = true;
-    this.idSelected = data.id;
-  }
-}
+export default class DrillDownPage extends Vue {}
 </script>
 
-<style lang="scss">
-@import '@/assets/scss/main.scss';
-
-.item-okrs {
-  .happy {
-    color: $green-primary-1;
-  }
-  .sad {
-    color: $red-primary-1;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
