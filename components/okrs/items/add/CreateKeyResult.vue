80131e05125ea390d5adefe9ffe24ea1d1d66abb
thaycacac <thaycacac@gmail.com>
2020-10-25 19:52:19 +0700

    update: key result

--- components/okrs/items/add/CreateKeyResult.vue
+++ components/okrs/items/add/CreateKeyResult.vue
@@ -1,19 +1,7 @@
 <template>
   <div class="add-krs-step">
     <div v-loading="formLoading">
-      <!-- <krs-form
-        v-for="(item, index) in krFormItems"
-        :key="index"
-        ref="krsForm"
-        :index-kr-form="index"
-        :key-result.sync="item"
-        @deleteKr="deleteKrForm($event)"
-      /> -->
-    </div>
-    <div class="-text-center">
-      <el-button class="el-button el-button--small el-button--purple" @click="addNewKRs">
-        Thêm KRs
-      </el-button>
+      <KeyResult />
     </div>
     <div class="add-krs-step__attention">
       <p class="add-krs-step__attention--title">Lưu ý:</p>
@@ -33,6 +21,7 @@
 import { Component, Vue, PropSync, Prop } from 'vue-property-decorator';
 import { Form } from 'element-ui';
 
+import KeyResult from './KeyResult.vue';
 import IconAttention from '@/assets/images/okrs/attention.svg';
 
 import { PayloadOkrs } from '@/constants/app.interface';
@@ -46,12 +35,7 @@ import OkrsRepository from '@/repositories/OkrsRepository';
   name: 'CreateObjectiveStep',
   components: {
     IconAttention,
-  },
-  created() {
-    this.objectiveTitle = this.$store.state.okrs.objective.title;
-    if (this.$store.state.okrs.keyResults) {
-      this.krFormItems = this.$store.state.okrs.keyResults.length !== 0 ? this.$store.state.okrs.keyResults.map((item) => ({ ...item })) : [];
-    }
+    KeyResult,
   },
   mounted() {
     console.log(this.$route.query.objectiveId);
@@ -61,26 +45,9 @@ export default class CreateObjectiveStep extends Vue {
   @PropSync('active', Number) private syncActive!: number;
   @PropSync('visibleDialog', Boolean) private syncVisibleDialog!: boolean;
 
-  private formLoading: boolean = false;
   private loading: boolean = false;
   private objectiveTitle: string = '';
-  private krFormItems: any[] = [];
   private attentionsText: string[] = ['Nên có ít nhất phải có 2 kết quả then chốt', 'Không nên quá 5 kết quả then chốt cho 1 mục tiêu'];
-
-  private addNewKRs() {
-    this.formLoading = true;
-    this.krFormItems.push({
-      startValue: 0,
-      targetValue: 100,
-      content: '',
-      linkPlans: '',
-      linkResults: '',
-      measureUnitId: 1,
-    });
-    setTimeout(() => {
-      this.formLoading = false;
-    }, 500);
-  }
 
   private closeKrsForm() {
     this.$confirm('Bạn có chắc chắn muốn thoát quá trình này không?', {
@@ -93,110 +60,110 @@ export default class CreateObjectiveStep extends Vue {
     });
   }
 
-  private async createRootOkrs() {
-    const payload: any = {};
-    const krs: any[] = [];
-    let validForm: number = 0;
+  // private async createRootOkrs() {
+  //   const payload: any = {};
+  //   const krs: any[] = [];
+  //   const validForm: number = 0;
 
-    this.loading = true;
-    if (this.krFormItems.length === 0) {
-      setTimeout(() => {
-        this.loading = false;
-      }, 300);
-      this.$message.error('Cần có ít nhất 1 kết quả then chốt');
-    } else {
-      (this.$refs.krsForm as any).forEach((form) => {
-        (form.$refs.keyResult as Form).validate((isValid: boolean, invalidatedFields: object) => {
-          if (isValid) {
-            validForm++;
-          }
-        });
-        krs.push(Object.freeze(form.syncTempKr));
-      });
-      if (validForm === krs.length) {
-        // Set this is root objective
-        const payload: PayloadOkrs = {
-          objective: Object.assign({}, this.$store.state.okrs.objective, { isRootObjective: true }),
-          keyResult: krs,
-        };
-        try {
-          await OkrsRepository.createOrUpdateOkrs(payload).then((res) => {
-            this.loading = false;
-            this.syncVisibleDialog = false;
-            this.krFormItems = [];
-            this.$store.dispatch(DispatchAction.CLEAR_OKRS);
-            this.$notify.success({
-              ...notificationConfig,
-              message: 'Tạo OKRs thành công',
-            });
-          });
-        } catch (error) {
-          this.loading = false;
-        }
-      } else {
-        setTimeout(() => {
-          this.loading = false;
-        }, 300);
-        this.$message.error('Vui lòng nhập đúng các trường yêu cầu');
-      }
-    }
-  }
+  //   this.loading = true;
+  // if (this.krFormItems.length === 0) {
+  //   setTimeout(() => {
+  //     this.loading = false;
+  //   }, 300);
+  //   this.$message.error('Cần có ít nhất 1 kết quả then chốt');
+  // } else {
+  //   (this.$refs.krsForm as any).forEach((form) => {
+  //     (form.$refs.keyResult as Form).validate((isValid: boolean, invalidatedFields: object) => {
+  //       if (isValid) {
+  //         validForm++;
+  //       }
+  //     });
+  //     krs.push(Object.freeze(form.syncTempKr));
+  //   });
+  //   if (validForm === krs.length) {
+  //     // Set this is root objective
+  //     const payload: PayloadOkrs = {
+  //       objective: Object.assign({}, this.$store.state.okrs.objective, { isRootObjective: true }),
+  //       keyResult: krs,
+  //     };
+  //     try {
+  //       await OkrsRepository.createOrUpdateOkrs(payload).then((res) => {
+  //         this.loading = false;
+  //         this.syncVisibleDialog = false;
+  //         this.krFormItems = [];
+  //         this.$store.dispatch(DispatchAction.CLEAR_OKRS);
+  //         this.$notify.success({
+  //           ...notificationConfig,
+  //           message: 'Tạo OKRs thành công',
+  //         });
+  //       });
+  //     } catch (error) {
+  //       this.loading = false;
+  //     }
+  //   } else {
+  //     setTimeout(() => {
+  //       this.loading = false;
+  //     }, 300);
+  //     this.$message.error('Vui lòng nhập đúng các trường yêu cầu');
+  //   }
+  // }
+  // }
 
   private backToStepOne() {
     this.$store.commit(MutationState.CLEAR_KRS);
-    if (this.krFormItems.length !== 0) {
-      const tempKrs: any[] = [];
-      (this.$refs.krsForm as any).forEach((form) => {
-        tempKrs.push(form.syncTempKr);
-      });
-      this.$store.commit(MutationState.SET_KRS, tempKrs);
-      this.syncActive--;
-    }
-    this.syncActive--;
+    // if (this.krFormItems.length !== 0) {
+    //   const tempKrs: any[] = [];
+    //   (this.$refs.krsForm as any).forEach((form) => {
+    //     tempKrs.push(form.syncTempKr);
+    //   });
+    //   this.$store.commit(MutationState.SET_KRS, tempKrs);
+    //   this.syncActive--;
+    // }
+    // this.syncActive--;
   }
 
-  private nextStepThree() {
-    const krs: any[] = [];
-    let validForm: number = 0;
+  // private nextStepThree() {
+  //   const krs: any[] = [];
+  //   let validForm: number = 0;
 
-    this.loading = true;
-    if (this.krFormItems.length === 0) {
-      setTimeout(() => {
-        this.loading = false;
-      }, 300);
-      this.$message.error('Cần có ít nhất 1 kết quả then chốt');
-    } else {
-      (this.$refs.krsForm as any).forEach((form) => {
-        (form.$refs.keyResult as Form).validate((isValid: boolean, invalidatedFields: object) => {
-          if (isValid) {
-            validForm++;
-          }
-        });
-        krs.push(Object.freeze(form.syncTempKr));
-      });
-      if (validForm === krs.length) {
-        // Set this is not root objective
-        const tempObjective = Object.assign({}, this.$store.state.okrs.objective, { isRootObjective: false });
-        this.$store.commit(MutationState.SET_KRS, krs);
-        this.$store.commit(MutationState.SET_OBJECTIVE, tempObjective);
-        this.syncActive++;
-        this.loading = false;
-      } else {
-        setTimeout(() => {
-          this.loading = false;
-        }, 300);
-        this.$message.error('Vui lòng nhập đúng các trường yêu cầu');
-      }
-    }
-  }
+  //   this.loading = true;
+  //   if (this.krFormItems.length === 0) {
+  //     setTimeout(() => {
+  //       this.loading = false;
+  //     }, 300);
+  //     this.$message.error('Cần có ít nhất 1 kết quả then chốt');
+  //   } else {
+  //     (this.$refs.krsForm as any).forEach((form) => {
+  //       (form.$refs.keyResult as Form).validate((isValid: boolean, invalidatedFields: object) => {
+  //         if (isValid) {
+  //           validForm++;
+  //         }
+  //       });
+  //       krs.push(Object.freeze(form.syncTempKr));
+  //     });
+  //     if (validForm === krs.length) {
+  //       // Set this is not root objective
+  //       const tempObjective = Object.assign({}, this.$store.state.okrs.objective, { isRootObjective: false });
+  //       this.$store.commit(MutationState.SET_KRS, krs);
+  //       this.$store.commit(MutationState.SET_OBJECTIVE, tempObjective);
+  //       this.syncActive++;
+  //       this.loading = false;
+  //     } else {
+  //       setTimeout(() => {
+  //         this.loading = false;
+  //       }, 300);
+  //       this.$message.error('Vui lòng nhập đúng các trường yêu cầu');
+  //     }
+  //   }
+  // }
 
-  private deleteKrForm(indexForm: number) {
-    this.formLoading = true;
-    this.krFormItems.splice(indexForm, 1);
-    setTimeout(() => {
-      this.formLoading = false;
-    }, 300);
-  }
+  // private deleteKrForm(indexForm: number) {
+  //   this.formLoading = true;
+  //   this.krFormItems.splice(indexForm, 1);
+  //   setTimeout(() => {
+  //     this.formLoading = false;
+  //   }, 300);
+  // }
 }
 </script>
 <style lang="scss" scoped>
--- components/okrs/items/add/KeyResult.vue
+++ components/okrs/items/add/KeyResult.vue
@@ -1,11 +1,12 @@
 <template>
-  <div :id="indexKrForm">
+  <div>
     <div :class="['control-collapse', hovering ? 'hovering' : '']" @mouseenter="hovering = true" @mouseleave="hovering = false">
       <div class="control-collapse__expand" @click="expandForm">
         <span :class="['control-collapse__expand--caret', 'el-icon-caret-right', isExpanded ? 'expanded' : '']" />
-        <span v-if="syncTempKr.content.length !== 0" class="control-collapse__expand--content">{{ syncTempKr.content }}</span>
-        <span v-if="syncTempKr.content.length === 0" class="control-collapse__expand--content example">Ấn vào đây để chỉnh sửa</span>
+        <span v-if="keyResultTemp.content.length !== 0" class="control-collapse__expand--content">{{ keyResultTemp.content }}</span>
+        <span v-if="keyResultTemp.content.length === 0" class="control-collapse__expand--content example">Ấn vào đây để chỉnh sửa</span>
       </div>
+
       <el-popover v-model="popoverVisisble" placement="top-start" width="200" trigger="click">
         <div class="control-collapse__popover">
           <p class="control-collapse__popover--title">Bạn muốn xóa KR này?</p>
@@ -20,9 +21,9 @@
       </el-popover>
     </div>
     <div :class="['content-collapse', isExpanded ? 'margin-form' : '']" :style="isExpanded ? `max-height: ${scrollHeight}px` : 'max-height: 0'">
-      <el-form ref="keyResult" :model="syncTempKr" :rules="rules" label-position="left" class="krs-form">
+      <el-form ref="keyResult" :model="keyResultTemp" :rules="rules" label-position="left" class="krs-form">
         <el-form-item prop="content">
-          <el-input v-model="syncTempKr.content" placeholder="Nhập kết quả then chốt" tabindex="1" />
+          <el-input v-model="keyResultTemp.content" placeholder="Nhập kết quả then chốt" tabindex="1" />
         </el-form-item>
         <div class="krs-form__detail">
           <div class="krs-form__detail--value">
@@ -30,7 +31,7 @@
               <el-col :span="7">
                 <el-form-item label="Đơn vị" class="custom-label" label-width="65px">
                   <el-select
-                    v-model.number="syncTempKr.measureUnitId"
+                    v-model.number="keyResultTemp.measureUnitId"
                     size="medium"
                     filterable
                     no-match-text="Không tìm thấy kết quả"
@@ -42,26 +43,31 @@
               </el-col>
               <el-col :span="7">
                 <el-form-item class="custom-label krs-form__detail--value--start" prop="startValue" label="Giá trị bắt đầu" label-width="100px">
-                  <el-input v-model.number="syncTempKr.startValue" size="medium" tabindex="3" />
+                  <el-input v-model.number="keyResultTemp.startValue" size="medium" tabindex="3" />
                 </el-form-item>
               </el-col>
               <el-col :span="10">
                 <el-form-item prop="targetValue" label="Mục tiêu" class="custom-label" label-width="80px">
-                  <el-input v-model.number="syncTempKr.targetValue" size="medium" tabindex="4" />
+                  <el-input v-model.number="keyResultTemp.targetValue" size="medium" tabindex="4" />
                 </el-form-item>
               </el-col>
             </el-row>
           </div>
           <div class="krs-form__detail--links">
             <el-form-item prop="linkPlans" label="Link kế hoạch" label-width="120px">
-              <el-input v-model.number="syncTempKr.linkPlans" size="small" type="url" placeholder="Điền link kế hoạch " />
+              <el-input v-model.number="keyResultTemp.linkPlans" size="small" type="url" placeholder="Điền link kế hoạch " />
             </el-form-item>
             <el-form-item prop="linkResults" label="Link kết quả" label-width="120px">
-              <el-input v-model.number="syncTempKr.linkResults" size="small" type="url" placeholder="Điền link kết quả" />
+              <el-input v-model.number="keyResultTemp.linkResults" size="small" type="url" placeholder="Điền link kết quả" />
             </el-form-item>
           </div>
         </div>
       </el-form>
+    </div>
+    <div class="-text-center">
+      <el-button class="el-button el-button--small el-button--purple" @click="addNewKRs">
+        Thêm KRs
+      </el-button>
     </div>
   </div>
 </template>
@@ -84,88 +90,81 @@ import { notificationConfig } from '@/constants/app.constant';
   },
 })
 export default class KrsForm extends Vue {
-  @PropSync('keyResult', {
-    type: Object,
-    required: true,
-    default: () => ({
-      startValue: 0,
-      targetValue: 100,
-      content: '',
-      linkPlans: '',
-      linkResults: '',
-      measureUnitId: 1,
-    }),
-  })
-  private syncTempKr!: any;
-
-  @Prop(Number) private indexKrForm!: number;
-
   private hovering: boolean = false;
   private popoverVisisble: boolean = false;
-  private isExpanded: boolean = false;
+  private isExpanded: boolean = true;
   private scrollHeight: number = 0;
   private tempContentKr: string = 'Ấn vào đây để chỉnh sửa';
 
+  private keyResultTemp: Object = {
+    startValue: 0,
+    targetValue: 100,
+    content: '',
+    linkPlans: '',
+    linkResults: '',
+    measureUnitId: 1,
+  };
+
   private units: any[] = [];
 
-  private async deleteKr(keyResult: any) {
-    if (keyResult.id) {
-      if (this.indexKrForm === 0) {
-        this.popoverVisisble = false;
-        this.$message.error('Cần có ít nhất 1 kết quả then chốt đã được tạo');
-      } else {
-        try {
-          await OkrsRepository.deleteKr(keyResult.id).then((res) => {
-            if (res.data.statusCode === 404) {
-              this.$notify.success({
-                ...notificationConfig,
-                message: res.data.message,
-              });
-              this.popoverVisisble = false;
-            }
-            if (res.data.statusCode === 480) {
-              this.$notify.success({
-                ...notificationConfig,
-                message: res.data.message,
-              });
-              this.popoverVisisble = false;
-            }
-            if (res.data.statusCode === 200) {
-              this.$notify.success({
-                ...notificationConfig,
-                message: 'Xóa KR thành công',
-              });
-              this.popoverVisisble = false;
-            }
-            this.$emit('deleteKr', this.indexKrForm);
-          });
-        } catch (error) {}
-      }
-    } else {
-      this.popoverVisisble = false;
-      this.$emit('deleteKr', this.indexKrForm);
-    }
-  }
+  // private async deleteKr(keyResult: any) {
+  //   if (keyResult.id) {
+  //     if (this.indexKrForm === 0) {
+  //       this.popoverVisisble = false;
+  //       this.$message.error('Cần có ít nhất 1 kết quả then chốt đã được tạo');
+  //     } else {
+  //       try {
+  //         await OkrsRepository.deleteKr(keyResult.id).then((res) => {
+  //           if (res.data.statusCode === 404) {
+  //             this.$notify.success({
+  //               ...notificationConfig,
+  //               message: res.data.message,
+  //             });
+  //             this.popoverVisisble = false;
+  //           }
+  //           if (res.data.statusCode === 480) {
+  //             this.$notify.success({
+  //               ...notificationConfig,
+  //               message: res.data.message,
+  //             });
+  //             this.popoverVisisble = false;
+  //           }
+  //           if (res.data.statusCode === 200) {
+  //             this.$notify.success({
+  //               ...notificationConfig,
+  //               message: 'Xóa KR thành công',
+  //             });
+  //             this.popoverVisisble = false;
+  //           }
+  //           this.$emit('deleteKr', this.indexKrForm);
+  //         });
+  //       } catch (error) {}
+  //     }
+  //   } else {
+  //     this.popoverVisisble = false;
+  //     this.$emit('deleteKr', this.indexKrForm);
+  //   }
+  // }
 
-  private rules: Maps<Rule[]> = {
-    content: [
-      { type: 'string', required: true, message: 'Vui lòng nhập kết quả then chốt', trigger: 'blur' },
-      { validator: this.validateContentKrs, trigger: 'blur' },
-      max255Char,
-    ],
-    startValue: [
-      { type: 'number', required: true, message: 'Vui lòng nhập giá trị', trigger: 'blur' },
-      { validator: this.validateIsValidNumber, trigger: 'blur' },
-      { validator: this.validateStartValue, trigger: 'blur' },
-    ],
-    targetValue: [
-      { type: 'number', required: true, message: 'Vui lòng nhập giá trị', trigger: 'blur' },
-      { validator: this.validateIsValidNumber, trigger: 'blur' },
-      { validator: this.validateTargetValue, trigger: 'blur' },
-    ],
-    linkPlans: [{ type: 'url', message: 'Vui lòng nhập đúng định dạng đường link', trigger: 'blur' }, max255Char],
-    linkResults: [{ type: 'url', message: 'Vui lòng nhập đúng định dạng đường link', trigger: 'blur' }, max255Char],
-  };
+  // private rules: Maps<Rule[]> = {
+  //   content: [
+  //     { type: 'string', required: true, message: 'Vui lòng nhập kết quả then chốt', trigger: 'blur' },
+  //     { validator: this.validateContentKrs, trigger: 'blur' },
+  //     max255Char,
+  //   ],
+  //   startValue: [
+  //     { type: 'number', required: true, message: 'Vui lòng nhập giá trị', trigger: 'blur' },
+  //     { validator: this.validateIsValidNumber, trigger: 'blur' },
+  //     { validator: this.validateStartValue, trigger: 'blur' },
+  //   ],
+  //   targetValue: [
+  //     { type: 'number', required: true, message: 'Vui lòng nhập giá trị', trigger: 'blur' },
+  //     { validator: this.validateIsValidNumber, trigger: 'blur' },
+  //     { validator: this.validateTargetValue, trigger: 'blur' },
+  //   ],
+  //   linkPlans: [{ type: 'url', message: 'Vui lòng nhập đúng định dạng đường link', trigger: 'blur' }, max255Char],
+  //   linkResults: [{ type: 'url', message: 'Vui lòng nhập đúng định dạng đường link', trigger: 'blur' }, max255Char],
+  // };
 
   private validateIsValidNumber(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
     if (isNaN(value)) {
@@ -174,25 +173,25 @@ export default class KrsForm extends Vue {
     return callback();
   }
 
-  private validateStartValue(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
-    if (value < 0) {
-      return callback('Giá trị phải là số không âm');
-    }
-    if (value > this.syncTempKr.targetValue) {
-      return callback('Giá trị bắt đầu đang lớn hơn giá trị mục tiêu');
-    }
-    return callback();
-  }
+  // private validateStartValue(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
+  //   if (value < 0) {
+  //     return callback('Giá trị phải là số không âm');
+  //   }
+  //   if (value > this.keyResultTemp.targetValue) {
+  //     return callback('Giá trị bắt đầu đang lớn hơn giá trị mục tiêu');
+  //   }
+  //   return callback();
+  // }
 
-  private validateTargetValue(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
-    if (value <= 0) {
-      return callback('Giá trị phải lớn hơn 0');
-    }
-    if (value < this.syncTempKr.startValue) {
-      return callback('Giá trị mục tiêu phải lớn hơn bắt đầu');
-    }
-    return callback();
-  }
+  // private validateTargetValue(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
+  //   if (value <= 0) {
+  //     return callback('Giá trị phải lớn hơn 0');
+  //   }
+  //   if (value < this.keyResultTemp.startValue) {
+  //     return callback('Giá trị mục tiêu phải lớn hơn bắt đầu');
+  //   }
+  //   return callback();
+  // }
 
   private validateContentKrs(rule: any, value: any, callback: (message?: string) => any): (message?: string) => any {
     if (!/\d/.test(value)) {
@@ -201,14 +200,29 @@ export default class KrsForm extends Vue {
     return callback();
   }
 
-  public clearForm() {
-    (this.$refs.keyResult as Form).clearValidate();
-    this.syncTempKr.content = '';
-    this.syncTempKr.targetvalue = 1;
-    this.syncTempKr.linkPlans = '';
-    this.syncTempKr.linkResults = '';
-    this.syncTempKr.measureUnitId = 1;
-  }
+  // private addNewKRs() {
+  //   this.formLoading = true;
+  //   this.krFormItems.push({
+  //     startValue: 0,
+  //     targetValue: 100,
+  //     content: '',
+  //     linkPlans: '',
+  //     linkResults: '',
+  //     measureUnitId: 1,
+  //   });
+  //   setTimeout(() => {
+  //     this.formLoading = false;
+  //   }, 500);
+  // }
+
+  // public clearForm() {
+  //   (this.$refs.keyResult as Form).clearValidate();
+  //   this.keyResultTemp.content = '';
+  //   this.keyResultTemp.targetvalue = 1;
+  //   this.keyResultTemp.linkPlans = '';
+  //   this.keyResultTemp.linkResults = '';
+  //   this.keyResultTemp.measureUnitId = 1;
+  // }
 
   private expandForm() {
     this.isExpanded = !this.isExpanded;
