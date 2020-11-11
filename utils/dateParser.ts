0c3706142dacb5cd1019fc0156b67dd854938250
ifuncoding <hnamnguyen.hy@gmail.com>
2020-11-12 00:41:27 +0700

    fix: fix date to add users

--- utils/dateParser.ts
+++ utils/dateParser.ts
@@ -34,5 +34,5 @@ export const formatDateFromExcel = (date): string => {
   if (month.length < 2) month = '0' + month;
   if (day.length < 2) day = '0' + day;
 
-  return [day, month, year].join('-');
+  return [day, month, year].join('/');
 };
