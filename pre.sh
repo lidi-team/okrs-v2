81ebab052d1a314e2517acf2f82f18c758fb207d
bluemarble <hoangledinh65@gmail.com>
2020-11-17 00:07:33 +0700

    today

--- pre.sh
+++ pre.sh
@@ -4,11 +4,8 @@ if test -d "$FILE"; then
 
     echo "file exists"
     killall node
-    whoami
     docker container stop my-frontend
     docker container prune -f
-    chmod 777 ./dist
-    rm -rf ./dist
 else 
     echo "file does not exist"
 fi
