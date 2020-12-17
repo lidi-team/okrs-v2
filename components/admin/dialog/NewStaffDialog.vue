3cad1669576e7d45f18ebd1e55c19539808210bf
ifuncoding <hnamnguyen.hy@gmail.com>
2020-12-18 00:49:44 +0700

    feat: UI manage staff by HR

---
+++ components/admin/dialog/NewStaffDialog.vue
@@ -0,0 +1,204 @@
+<template>
+  <el-dialog
+    class="update-employee"
+    :visible.sync="syncStaffDialog"
+    width="45%"
+    placement="bottom-start"
+    title="Thêm một nhân viên"
+    :before-close="handleCloseDialog"
+  >
+    <el-form
+      ref="employee"
+      v-loading="loadingForm"
+      :model="employee"
+      label-width="180px"
+      label-position="left"
+      :rules="rules"
+    >
+      <el-form-item prop="email" label="Email" class="custom-label">
+        <el-input
+          v-model="employee.email"
+          size="medium"
+          class="register-form__input__email"
+          placeholder="Nhập email"
+          type="email"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="fullName" label="Họ và tên" class="custom-label">
+        <el-input
+          v-model="employee.fullName"
+          size="medium"
+          class="register-form__input__full-name"
+          placeholder="Nhập họ tên"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="dob" label="Ngày sinh" class="custom-label">
+        <el-date-picker
+          v-model="employee.dob"
+          type="date"
+          placeholder="Chọn ngày sinh dd/MM/yyyy"
+          :format="dateFormat"
+          :value-format="dateFormat"
+          :picker-options="pickerOptions"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item
+        prop="teamId"
+        label="Phòng ban"
+        class="register-form__input__team custom-label"
+      >
+        <el-select
+          v-model="employee.departmentId"
+          size="medium"
+          placeholder="Chọn phòng ban"
+          :no-data-text="noDataText"
+        >
+          <el-option
+            v-for="item in departments"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item prop="gender" label="Giới tính">
+        <el-radio-group v-model="employee.gender">
+          <el-radio :label="1">Nam</el-radio>
+          <el-radio :label="0">Nữ</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item class="create-member-dialog__action">
+        <el-button
+          class="el-button--purple el-button--modal"
+          :loading="loadingForm"
+          @click="createEmployee"
+          >Thêm nhân viên
+        </el-button>
+        <el-button class="el-button--modal" @click="handleCloseDialog"
+          >Hủy
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Prop, PropSync, Vue } from 'vue-property-decorator';
+import { EmployeeDTO } from '../../../constants/app.interface';
+import { Form } from 'element-ui';
+import { compareTwoDate } from '@/utils/dateParser';
+import { Maps, Rule } from '@/constants/app.type';
+import { max255Char } from '@/constants/account.constant';
+import EmployeeRepository from '@/repositories/EmployeeRepository';
+
+@Component<StaffDialog>({
+  name: 'StaffDialog',
+})
+export default class StaffDialog extends Vue {
+  @Prop(Array) readonly departments!: Array<any>;
+  @PropSync('visibleDialog', { type: Boolean, required: true })
+  public syncStaffDialog!: boolean;
+  private loadingForm: Boolean = false;
+  private noDataText: string = 'Không có dữ liệu';
+  private dateFormat: string = 'dd/MM/yyyy';
+
+  private employee: EmployeeDTO = {
+    fullName: '',
+    email: '',
+    roles: [],
+    departmentId: null,
+    gender: 1,
+    dob: '',
+  };
+  private handleCloseDialog() {
+    const updateUserForm = this.$refs.employee as Form;
+    updateUserForm.clearValidate();
+    this.syncStaffDialog = false;
+  }
+
+  private clearForm(): void {
+    this.employee = {
+      fullName: '',
+      email: '',
+      roles: [],
+      departmentId: null,
+      gender: 1,
+      dob: '',
+    };
+  }
+
+  private validateEndDate(
+    rule: any,
+    value: any,
+    callback: (message?: string) => any,
+  ): (message?: string) => any {
+    if (compareTwoDate(value, new Date().toString()) === 1) {
+      return callback('Ngày sinh phải nhỏ hơn ngày hiện tại');
+    }
+    return callback();
+  }
+
+  private rules: Maps<Rule[]> = {
+    email: [
+      {
+        required: true,
+        message: 'Vui lòng nhập địa chỉ email',
+        trigger: 'blur',
+      },
+      {
+        type: 'email',
+        message: 'Vui lòng nhập đúng địa chỉ email',
+        trigger: 'blur',
+      },
+      max255Char,
+    ],
+    fullName: [{ required: true, message: 'Vui lòng nhập họ tên' }, max255Char],
+    departmentId: [{ required: true, message: 'Vui lòng chọn phòng ban' }],
+    dob: [
+      {
+        required: true,
+        message: 'Vui lòng chọn ngày sinh',
+        trigger: ['blur', 'change'],
+      },
+      { validator: this.validateEndDate, trigger: ['blur', 'change'] },
+    ],
+  };
+
+  private createEmployee() {
+    console.log('Employee', this.employee);
+    (this.$refs.employee as Form).validate(
+      async (isValid: boolean, invalidatedFields: object) => {
+        if (isValid) {
+          this.loadingForm = true;
+          try {
+            const response = await EmployeeRepository.create({
+              users: [this.employee],
+            });
+            console.log(response);
+            if (response) {
+              this.clearForm();
+              this.loadingForm = false;
+            }
+          } catch (error) {
+            this.loadingForm = false;
+            console.log(error);
+          }
+        }
+        if (invalidatedFields) {
+          setTimeout(() => {
+            this.loadingForm = false;
+          }, 300);
+        }
+      },
+    );
+  }
+
+  private pickerOptions: any = {
+    disabledDate(time) {
+      return time.getTime() > Date.now();
+    },
+  };
+}
+</script>
+
+<style scoped></style>
--- pages/nhan-su/them/index.vue
+++ pages/nhan-su/them/index.vue
@@ -1,171 +1,110 @@
 <template>
-  <div class="create-employee">
+  <div>
     <el-page-header title="Quay lại" @back="goBack" />
-    <div class="okrs-detail--top-action">
-      <span class="okrs-detail--top-action__title">Thêm nhân viên</span>
+    <div class="box-wrap">
+      <div class="-display-flex -justify-content-between -border-header">
+        <span class="-title-2">Thêm nhân viên</span>
+        <div>
+          <el-button
+            class="el-button--purple el-button--invite"
+            size="small"
+            @click="handleShowDialog"
+          >
+            Thêm một nhân viên
+          </el-button>
+        </div>
+      </div>
+      <upload-excel-component
+        :on-success="handleSuccess"
+        :before-upload="beforeUpload"
+      />
+      <el-button
+        class="el-button--purple el-button--modal create-employee__button"
+        icon="el-icon-plus"
+        :disabled="!hasData"
+        @click="handleAddEmployee"
+        >Thêm
+      </el-button>
+      <el-table
+        :data="tableData"
+        border
+        highlight-current-row
+        style="width: 100%; margin-top: 20px"
+        fit
+      >
+        <!-- <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" /> -->
+        <el-table-column
+          label="Email"
+          prop="email"
+          align="center"
+          min-width="80"
+        >
+          <template slot-scope="{ row }">
+            <span>{{ row.email }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="Họ và tên"
+          prop="fullName"
+          align="center"
+          min-width="80"
+        >
+          <template slot-scope="{ row }">
+            <span>{{ row.fullName }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="Ngày sinh"
+          prop="dob"
+          align="center"
+          min-width="80"
+        >
+          <template slot-scope="{ row }">
+            <span>{{ row.dob }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="Số điện thoại"
+          prop="phoneNumber"
+          align="center"
+          min-width="80"
+        >
+          <template slot-scope="{ row }">
+            <span>{{ row.phoneNumber }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="Giới tính"
+          prop="gender"
+          align="center"
+          min-width="80"
+        >
+          <template slot-scope="{ row }">
+            <span>{{ row.gender }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="Phòng ban"
+          prop="department"
+          align="center"
+          min-width="80"
+        >
+          <template slot-scope="{ row }">
+            <span>{{ row.department }}</span>
+          </template>
+        </el-table-column>
+        <!-- <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
+        <template slot-scope="{ row, $index }">
+          <el-button type="primary" size="mini" @click="handleUpdate(row)"> Sửa </el-button>
+          <el-button v-if="row.status != 'deleted'" size="mini" type="danger" @click="handleDelete(row, $index)"> Xóa </el-button>
+        </template>
+      </el-table-column> -->
+      </el-table>
     </div>
-    <el-tabs v-model="tabActive" type="border-card" @tab-click="handleClick">
-      <el-tab-pane label="Tải lên file" name="excel">
-        <upload-excel-component
-          :on-success="handleSuccess"
-          :before-upload="beforeUpload"
-        />
-        <el-button
-          class="el-button--purple el-button--modal create-employee__button"
-          :disabled="!hasData"
-          @click="handleAddEmployee"
-          >Thêm nhân viên</el-button
-        >
-        <el-table
-          :data="tableData"
-          border
-          highlight-current-row
-          style="width: 100%; margin-top: 20px"
-          fit
-        >
-          <!-- <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" /> -->
-          <el-table-column
-            label="Email"
-            prop="email"
-            align="center"
-            min-width="80"
-          >
-            <template slot-scope="{ row }">
-              <span>{{ row.email }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="Họ và tên"
-            prop="fullName"
-            align="center"
-            min-width="80"
-          >
-            <template slot-scope="{ row }">
-              <span>{{ row.fullName }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="Ngày sinh"
-            prop="dob"
-            align="center"
-            min-width="80"
-          >
-            <template slot-scope="{ row }">
-              <span>{{ row.dob }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="Số điện thoại"
-            prop="phoneNumber"
-            align="center"
-            min-width="80"
-          >
-            <template slot-scope="{ row }">
-              <span>{{ row.phoneNumber }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="Giới tính"
-            prop="gender"
-            align="center"
-            min-width="80"
-          >
-            <template slot-scope="{ row }">
-              <span>{{ row.gender }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="Phòng ban"
-            prop="department"
-            align="center"
-            min-width="80"
-          >
-            <template slot-scope="{ row }">
-              <span>{{ row.department }}</span>
-            </template>
-          </el-table-column>
-          <!-- <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
-            <template slot-scope="{ row, $index }">
-              <el-button type="primary" size="mini" @click="handleUpdate(row)"> Sửa </el-button>
-              <el-button v-if="row.status != 'deleted'" size="mini" type="danger" @click="handleDelete(row, $index)"> Xóa </el-button>
-            </template>
-          </el-table-column> -->
-        </el-table>
-      </el-tab-pane>
-      <el-tab-pane label="Thêm một nhân viên" name="one">
-        <!-- FORM -->
-        <el-form
-          ref="employee"
-          v-loading="loadingForm"
-          :model="employee"
-          label-width="180px"
-          label-position="left"
-          :rules="rules"
-        >
-          <el-form-item prop="email" label="Email" class="custom-label">
-            <el-input
-              v-model="employee.email"
-              size="medium"
-              class="register-form__input__email"
-              placeholder="Nhập email"
-              type="email"
-            ></el-input>
-          </el-form-item>
-          <el-form-item prop="fullName" label="Họ và tên" class="custom-label">
-            <el-input
-              v-model="employee.fullName"
-              size="medium"
-              class="register-form__input__full-name"
-              placeholder="Nhập họ tên"
-            ></el-input>
-          </el-form-item>
-          <el-form-item prop="dob" label="Ngày sinh" class="custom-label">
-            <el-date-picker
-              v-model="employee.dob"
-              type="date"
-              placeholder="Chọn ngày sinh dd/MM/yyyy"
-              :format="dateFormat"
-              :value-format="dateFormat"
-              :picker-options="pickerOptions"
-            ></el-date-picker>
-          </el-form-item>
-          <el-form-item
-            prop="teamId"
-            label="Phòng ban"
-            class="register-form__input__team custom-label"
-          >
-            <el-select
-              v-model="employee.departmentId"
-              size="medium"
-              placeholder="Chọn phòng ban"
-              :no-data-text="noDataText"
-            >
-              <el-option
-                v-for="item in departments"
-                :key="item.id"
-                :label="item.name"
-                :value="item.id"
-              />
-            </el-select>
-          </el-form-item>
-          <el-form-item prop="gender" label="Giới tính">
-            <el-radio-group v-model="employee.gender">
-              <el-radio :label="1">Nam</el-radio>
-              <el-radio :label="0">Nữ</el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item class="create-member-dialog__action">
-            <el-button
-              class="el-button--purple el-button--modal"
-              :loading="loading"
-              @click="createEmployee"
-              >Thêm nhân viên
-            </el-button>
-          </el-form-item>
-        </el-form>
-        <!-- END FORM -->
-      </el-tab-pane>
-    </el-tabs>
+    <staff-dialog
+      :visible-dialog.sync="visibleDialog"
+      :departments="departments"
+    />
   </div>
 </template>
 
@@ -173,22 +112,16 @@
 import { Component, Vue, Watch } from 'vue-property-decorator';
 import UploadExcelComponent from '@/components/UploadExcel/index.vue';
 import { compareTwoDate, formatDateFromExcel } from '@/utils/dateParser';
-import { dateFormat } from '@vuejs-community/vue-filter-date-format';
 import TeamRepository from '@/repositories/TeamRepository';
-import value from '*.png';
 import EmployeeRepository from '@/repositories/EmployeeRepository';
-import {
-  notificationConfig,
-  confirmWarningConfig,
-} from '@/constants/app.constant';
 import { EmployeeDTO } from '@/constants/app.interface';
 import { Maps, Rule } from '@/constants/app.type';
 import { max255Char } from '@/constants/account.constant';
-import { Form } from 'element-ui';
+import StaffDialog from '@/components/admin/dialog/NewStaffDialog.vue';
 
 @Component<CreateEmployee>({
   name: 'CreateEmployee',
-  components: { UploadExcelComponent },
+  components: { UploadExcelComponent, StaffDialog },
   async created() {
     await this.getDataCommons();
   },
@@ -201,8 +134,7 @@ export default class CreateEmployee extends Vue {
   private hasData: boolean = false;
   private loading: boolean = false;
   private loadingForm: Boolean = false;
-  private noDataText: string = 'Không có dữ liệu';
-  private dateFormat: string = 'dd/MM/yyyy';
+  private visibleDialog: Boolean = false;
 
   private employee: EmployeeDTO = {
     fullName: '',
@@ -368,11 +300,16 @@ export default class CreateEmployee extends Vue {
   private goBack() {
     this.$router.go(-1);
   }
+
+  private handleShowDialog() {
+    this.visibleDialog = true;
+  }
 }
 </script>
 
 <style lang="scss">
 @import '@/assets/scss/main.scss';
+
 .create-employee {
   &__button {
     margin: 20px 0;
