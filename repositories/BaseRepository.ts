2ebaf0c0737a5d7f67380bf662b60cf91b0b856d
Phạm Ngọc Hòa <thaycacac@gmail.com>
2020-12-28 01:10:34 +0700

    Update BaseRepository.ts

--- repositories/BaseRepository.ts
+++ repositories/BaseRepository.ts
@@ -37,9 +37,12 @@ baseUrl.interceptors.response.use(
   },
   (error) => {
     switch (error.response.status) {
-      // case 401:
-       // window.location.href = '/dang-nhap';
-       // break;
+       case 401:
+        Notification.error({
+          ...notificationConfig,
+          message: error.response.data.message,
+        });
+        break;
       case 403:
         window.location.href = '/403';
         break;
